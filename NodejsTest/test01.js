5492691a7b7a6bc921939946a40aa906f18c7a5d
ChangerHe <244737162@qq.com>
2017-07-28 22:04:16 +0800

    daily text

---
+++ NodejsTest/test01.js
@@ -0,0 +1,7 @@
+
+function sayHi() {
+	console.log("Hi")
+}
+
+var hello = require("./test02.js");
+// hello()
---
+++ NodejsTest/test02.js
@@ -0,0 +1,8 @@
+// function sayHello() {
+// 	console.log("Hello")
+// }
+// exports.a = sayHello;
+
+
+var s = "Changer";
+console.log(s)
