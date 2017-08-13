54bd32c189b4abafc86b9dc0fa759fea8e33f297
ChangerHe <244737162@qq.com>
2017-08-13 23:43:22 +0800

    day1

--- MVCstructs/base.js
+++ MVCstructs/base.js
@@ -1,1 +1,13 @@
 //连缀的写法,可以避免写过多的代码,更加的方便
+
+var Base = {
+    getId: function(id) {
+        return document.getElementById(id)
+    },
+    getName: function(name) {
+        return document.getElementsByName(name)
+    },
+    getTagName: function(tag) {
+        return document.getElementsByTagName(tag)
+    }
+}
