5d1efd585f67fe2256344ebaa009a9f404728e87
heqingchang <heqc@udiannet.com>
2018-03-20 20:16:57 +0800

    feat: 完成animated的动画demo

--- reactNativeDemoV54/App.js
+++ reactNativeDemoV54/App.js
@@ -1,58 +1,126 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {StackNavigator} from 'react-navigation'
-import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  NavigatorIOS,
+  TouchableHighlight
+} from 'react-native';
 import Greeting from './components/Greeting'
 import Blink from './components/Blink'
 import FlexBlock from './components/FlexBlock'
-// import InputBlock from './components/InputBlock'
+import InputBlock from './components/InputBlock'
 import ButtonEvent from './components/ButtonEvent'
 import FlatListCom from './components/FlatListCom'
+import AnimatedShow from './components/AnimatedShow'
 
-export default App = StackNavigator({
-  Main: {screen: MainScreen},
-  Profile: {
-    screen: ProfileScreen
-  }
-})
+// const App = StackNavigator({
+//   Main: {screen:MainScreen},
+//   Profile: {screen:ProfileScreen}
+// })
 
-class MainScreen extends React.Component {
-  static navigationOptions = {
-    title: 'welcome'
-  }
+// class MainScreen extends React.Component {
+//   static navigationOptions = {
+//     title: 'Welcome'
+//   }
+//   render() {
+//     const {navigate} = this.props.navigation 
+//     return (
+//       <Button
+//         title='goto profile'
+//         onPress = {
+//           () => {
+//             navigate('Profile', {name: 'Jane'})
+//           }
+//         }
+//       ></Button>
+//     )
+//   }
+// }
+
+// class ProfileScreen extends React.Component {
+//   static navigationOptions = {
+//     title: 'Welcome ProfileScreen'
+//   }
+//   render() {
+//     const {navigate} = this.props.navigation 
+//     return (
+//       <Button
+//         title='goto profile'
+//         onPress = {
+//           () => {
+//             navigate('Main', {name: 'Jane'})
+//           }
+//         }
+//       ></Button>
+//     )
+//   }
+// }
+
+// class MyScene extends Component {
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     navigator: PropTypes.object.isRequired,
+//   }
+
+//   constructor(props, context) {
+//     super(props, context);
+//     this._onForward = this._onForward.bind(this);
+//   }
+
+//   _onForward() {
+//     this.props.navigator.push({
+//       title: 'Scene ' + nextIndex,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <View>
+//         <Text>Current Scene: { this.props.title }</Text>
+//         <TouchableHighlight onPress={this._onForward}>
+//           <Text>Tap me to load the next scene</Text>
+//         </TouchableHighlight>
+//       </View>
+//     )
+//   }
+// }
+
+export default class App extends React.Component {
   render() {
-    const {navigate} = this.props.navigation
     let pic = {
 uri : 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     }
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Text onPress = {
-            () => {
-              navigate('Profile', {name: 'jane'})
-            }
-          } style={styles.red}>Open up App.js to start working on your app!</Text>
+          <Text style={styles.red}>Open up App.js to start working on your app!</Text>
           <Text style={styles.bigBlue}>Changes you make will automatically reload.</Text>
           <Text style={[styles.bigBlue, styles.red]}>Shake your phone to open the developer menu.</Text>
           <Text>hello world1111</Text>
           <Image source={pic} style={{width: 193, height: 110}}/>
           <Greeting userName='Changer He'/>
           <Blink text='lorem adadaddadadadadasda'/>
-          {/* <FlexBlock></FlexBlock> */}
-          {/* <InputBlock></InputBlock> */}
-          {/* <ButtonEvent/> */}
-          {/* <FlatListCom/> */}
+          <FlexBlock></FlexBlock>
+          <InputBlock></InputBlock>
+          <ButtonEvent/>
+          <FlatListCom/>
+          {/* 页面的跳转, 未实现成功 */}
+          {/* <MainScreen/> */}
+          {/* 图片的加载, 未实现成功 */}
+          <Image source={require('./img/1.jpg')}  style={{width: 400, height: 400}}/>
+        {/* <AnimatedShow/> */}
+        
         </View>
       </ScrollView>
+      // 此动画必须在组件的根目录下设置, 否则无效
+      // <AnimatedShow style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+      //   <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
+      // </AnimatedShow>
     );
-  }
-}
-
-class ProfileScreen extends Component {
-  render() {
-    return (
-      <View>ProfileScreen</View>
-    )
   }
 }
 
--- reactNativeDemoV54/App_backup.js
+++ reactNativeDemoV54/App_backup.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {StackNavigator} from 'react-navigation'
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    ScrollView,
-    NavigatorIOS,
-    TouchableHighlight
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  NavigatorIOS,
+  TouchableHighlight
 } from 'react-native';
 import Greeting from './components/Greeting'
 import Blink from './components/Blink'
@@ -17,101 +17,134 @@ import ButtonEvent from './components/ButtonEvent'
 import FlatListCom from './components/FlatListCom'
 
 const App = StackNavigator({
-    Main: {
-        screen: MainScreen
-    },
-    Profile: {
-        screen: ProfileScreen
-    }
+  Main: {
+    screen: MainScreen
+  },
+  Profile: {
+    screen: ProfileScreen
+  }
 })
 
-// class MainScreen extends React.Component {   static navigationOptions = {
-// title: 'Welcome'   }   render() {     const {navigate} =
-// this.props.navigation     return (       <Button         title='goto profile'
-//         onPress = {           () => {             navigate('Profile', {name:
-// 'Jane'})           }         }       ></Button>     )   } } class
-// ProfileScreen extends React.Component {   static navigationOptions = {
-// title: 'Welcome ProfileScreen'   }   render() {     const {navigate} =
-// this.props.navigation     return (       <Button         title='goto profile'
-//         onPress = {           () => {             navigate('Main', {name:
-// 'Jane'})           }         }       ></Button>     )   } }
+// class MainScreen extends React.Component {
+//   static navigationOptions = {
+//     title: 'Welcome'
+//   }
+//   render() {
+//     const {navigate} = this.props.navigation 
+//     return (
+//       <Button
+//         title='goto profile'
+//         onPress = {
+//           () => {
+//             navigate('Profile', {name: 'Jane'})
+//           }
+//         }
+//       ></Button>
+//     )
+//   }
+// }
+
+// class ProfileScreen extends React.Component {
+//   static navigationOptions = {
+//     title: 'Welcome ProfileScreen'
+//   }
+//   render() {
+//     const {navigate} = this.props.navigation 
+//     return (
+//       <Button
+//         title='goto profile'
+//         onPress = {
+//           () => {
+//             navigate('Main', {name: 'Jane'})
+//           }
+//         }
+//       ></Button>
+//     )
+//   }
+// }
 
 class MyScene extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        navigator: PropTypes.object.isRequired
-    }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigator: PropTypes.object.isRequired,
+  }
 
-    constructor(props, context) {
-        super(props, context);
-        this._onForward = this
-            ._onForward
-            .bind(this);
-    }
+  constructor(props, context) {
+    super(props, context);
+    this._onForward = this._onForward.bind(this);
+  }
 
-    _onForward() {
-        this
-            .props
-            .navigator
-            .push({
-                title: 'Scene ' + nextIndex
-            });
-    }
+  _onForward() {
+    this.props.navigator.push({
+      title: 'Scene ' + nextIndex,
+    });
+  }
 
-    render() {
-        return (
-            <View>
-                <Text>Current Scene: {this.props.title}</Text>
-                <TouchableHighlight onPress={this._onForward}>
-                    <Text>Tap me to load the next scene</Text>
-                </TouchableHighlight>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this._onForward}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
 }
 
 export default class App extends React.Component {
-    render() {
-        //     let pic = { uri :
-        // 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        // }
-        return (
-            <NavigatorIOS
-                initialRoute={{
-                component: MyScene,
-                title: 'My Initial Scene',
-                passProps: {
-                    myProp: 'foo'
-                }
-            }}/>
-        // <ScrollView>   <View style={styles.container}>     <Text
-        // style={styles.red}>Open up App.js to start working on your app!</Text>
-        // <Text style={styles.bigBlue}>Changes you make will automatically
-        // reload.</Text>     <Text style={[styles.bigBlue, styles.red]}>Shake your
-        // phone to open the developer menu.</Text>     <Text>hello world1111</Text>
-        // <Image source={pic} style={{width: 193, height: 110}}/>     <Greeting
-        // userName='Changer He'/>     <Blink text='lorem adadaddadadadadasda'/>
-        // <FlexBlock></FlexBlock>     <InputBlock></InputBlock>     <ButtonEvent/>
-        // <FlatListCom/>     <MainScreen/>     <NavigatorIOS       initialRoute={{
-        //    component: MyScene,         title: 'My Initial Scene',         passProps:
-        // { myProp: 'foo' },       }}     />   </View> </ScrollView>
-        );
-    }
+  render() {
+//     let pic = {
+// uri : 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     }
+    return (
+      <NavigatorIOS
+            initialRoute={{
+              component: MyScene,
+              title: 'My Initial Scene',
+              passProps: { myProp: 'foo' },
+            }}
+          />
+      // <ScrollView>
+      //   <View style={styles.container}>
+      //     <Text style={styles.red}>Open up App.js to start working on your app!</Text>
+      //     <Text style={styles.bigBlue}>Changes you make will automatically reload.</Text>
+      //     <Text style={[styles.bigBlue, styles.red]}>Shake your phone to open the developer menu.</Text>
+      //     <Text>hello world1111</Text>
+      //     <Image source={pic} style={{width: 193, height: 110}}/>
+      //     <Greeting userName='Changer He'/>
+      //     <Blink text='lorem adadaddadadadadasda'/>
+      //     <FlexBlock></FlexBlock>
+      //     <InputBlock></InputBlock>
+      //     <ButtonEvent/>
+      //     <FlatListCom/>
+      //     <MainScreen/>
+      //     <NavigatorIOS
+      //       initialRoute={{
+      //         component: MyScene,
+      //         title: 'My Initial Scene',
+      //         passProps: { myProp: 'foo' },
+      //       }}
+      //     />
+      //   </View>
+      // </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    bigBlue: {
-        color: 'blue',
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    red: {
-        color: 'red'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  red: {
+    color: 'red'
+  },
 });
